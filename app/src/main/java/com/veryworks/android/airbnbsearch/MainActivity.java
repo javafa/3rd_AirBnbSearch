d2704c7572a4d24244a2d333ad53e8f20c3620db
javafa <nigne2016@gmail.com>
2017-07-26 14:56:13 +0900

    Initial commit

---
+++ app/src/main/java/com/veryworks/android/airbnbsearch/MainActivity.java
@@ -0,0 +1,45 @@
+package com.veryworks.android.airbnbsearch;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    Button btnCheckin, btnCheckout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnCheckin = (Button) findViewById(R.id.btnCheckin);
+        String btnCheckinText = getString(R.string.hint_start_date)
+                +"<br> <font color=\"#fd5a5f\">"+ getString(R.string.hint_select_date)+"</font>";
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.N){
+            btnCheckin.setText(Html.fromHtml(btnCheckinText), TextView.BufferType.SPANNABLE);
+        }else {
+            btnCheckin.setText(Html.fromHtml(btnCheckinText, Html.FROM_HTML_MODE_COMPACT), TextView.BufferType.SPANNABLE);
+        }
+        btnCheckout = (Button) findViewById(R.id.btnCheckout);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
