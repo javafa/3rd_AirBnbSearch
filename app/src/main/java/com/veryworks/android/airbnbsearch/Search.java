99497445ce8fe2fe2abe3adf01b569db3c942434
javafa <nigne2016@gmail.com>
2017-07-27 11:35:03 +0900

    add calendarView listener

---
+++ app/src/main/java/com/veryworks/android/airbnbsearch/Search.java
@@ -0,0 +1,14 @@
+package com.veryworks.android.airbnbsearch;
+
+/**
+ * Created by pc on 7/27/2017.
+ */
+
+public class Search {
+    public String checkinDate = null;
+    public String checkoutDate = null;
+    public int guest;
+    public int type;
+    public int price;
+    public String amenitie[];
+}
